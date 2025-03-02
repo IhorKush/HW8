0dc160be14d1a681f242a21a9a92a6bb0ba5669a
Ihor Yakushev <ihor.yakushev33@mail.com>
2025-03-02 14:14:35 +0200

    Task1 is solved.

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+        int[] array = {5, 19, 32, 11, 56, 49, 77, 90, 26, 85};
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
 
+        System.out.println("\n");
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
 
+        System.out.println("\n");
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+        }
+
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
     }
 }
